48d30c2bdd7ff2081abcd8b40589ff595b2be28a
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-15 16:55:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,54 @@
+# class Artist
+# 
+#   attr_accessor :name, :genre, :artist
+# 
+#   @@all = []
+# 
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def new_song(name, genre)
+#     song = Song.new(name, self, genre)
+#   end
+# 
+#   def songs
+#     Song.all.select do |song|
+#       song.artist == self
+#     end
+#   end
+# 
+#   def genres
+#     Songs.all.map do |song|
+#       song.genre
+#     end
+#   end
+# end
+
+
 class Artist
 
-  attr_accessor :name, :genre, :artist
+  attr_accessor :name, :artist, :genre
 
-  @@all = []
+  ALL=[]
 
   def initialize(name)
     @name = name
-    @@all << self
+    ALL << self
   end
 
   def self.all
-    @@all
+    ALL
   end
 
   def new_song(name, genre)
     song = Song.new(name, self, genre)
+    self.songs.last
   end
 
   def songs
@@ -24,8 +58,9 @@ class Artist
   end
 
   def genres
-    Songs.all.map do |song|
+    Song.all.map do |song|
       song.genre
     end
   end
+
 end
